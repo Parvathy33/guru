4943b4c777086a84c31a3a0701bd0371da2d72bc
Parvathy Premkumar <parvmithra@gmail.com>
2026-02-12 23:31:57 +0530

    Run Chrome in headless mode for Jenkins stability

--- src/main/java/guru99bank/base/BaseTest.java
+++ src/main/java/guru99bank/base/BaseTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -13,39 +14,65 @@ import guru99bank.constants.CommonConstants;
 import guru99bank.utilities.ConfigReader;
 
 public class BaseTest {
-	public static WebDriver driver;
-	
-	@BeforeMethod
-	public void setUp() {
-		String strBrowser = ConfigReader.getPropertyFromKey("browser");
-		System.out.println("'" + strBrowser + "' browser selected for test.");
-		String url = ConfigReader.getPropertyFromKey("url");
-		System.out.println("Application under test : '" + url + "'.");
-		
-		if (strBrowser.equalsIgnoreCase("Chrome")) {
-			driver = new ChromeDriver();
-		} else if (strBrowser.equalsIgnoreCase("Firefox")) {
-			driver = new FirefoxDriver();
-		} else if (strBrowser.equalsIgnoreCase("Edge")) {
-			driver = new EdgeDriver();
-		} else {
-			System.out.println("Unsupported browser type mentioned in the config.properties file.");
-			System.out.println("Edge browser selected for test as default.");
-			driver = new EdgeDriver();
-		}
-		
-		System.out.println("'" + strBrowser + "' browser initialized.");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonConstants.getImplicitWaitTime()));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonConstants.getPageLoadTimeOut()));
-		driver.manage().window().maximize();
-		driver.get(url);
-		System.out.println("Navigated to url '" + url + "'.");
-	}
 
-	@AfterMethod
-	public void tearDown() {
-		driver.quit();
-		System.out.println("All '" + ConfigReader.getPropertyFromKey("browser") + "' browser instances closed.");
-	}
+    public static WebDriver driver;
 
+    @BeforeMethod
+    public void setUp() {
+
+        String strBrowser = ConfigReader.getPropertyFromKey("browser");
+        String url = ConfigReader.getPropertyFromKey("url");
+
+        System.out.println("'" + strBrowser + "' browser selected for test.");
+        System.out.println("Application under test : '" + url + "'.");
+
+        boolean isJenkins = System.getenv("JENKINS_HOME") != null;
+
+        if (strBrowser.equalsIgnoreCase("Chrome")) {
+
+            ChromeOptions options = new ChromeOptions();
+
+            if (isJenkins) {
+                System.out.println("Running in Jenkins → Headless Chrome");
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
+            }
+
+            driver = new ChromeDriver(options);
+
+        } else if (strBrowser.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+
+        } else if (strBrowser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+
+        } else {
+            System.out.println("Unsupported browser type. Defaulting to Edge.");
+            driver = new EdgeDriver();
+        }
+
+        System.out.println("'" + strBrowser + "' browser initialized.");
+
+        driver.manage().timeouts().implicitlyWait(
+                Duration.ofSeconds(CommonConstants.getImplicitWaitTime()));
+        driver.manage().timeouts().pageLoadTimeout(
+                Duration.ofSeconds(CommonConstants.getPageLoadTimeOut()));
+
+        if (!isJenkins) {
+            driver.manage().window().maximize();
+        }
+
+        driver.get(url);
+        System.out.println("Navigated to url '" + url + "'.");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("All browser instances closed.");
+        }
+    }
 }
